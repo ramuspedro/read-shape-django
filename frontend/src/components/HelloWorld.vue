ecb09f61c5ce5006ee5d9a0e36e574c500f347ae
Pedro Ramos <pedro.ramos@terras.agr.br>
2018-10-25 17:36:47 -0300

    possible to load a shape on maop

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -9,7 +9,7 @@
         <div class="col-md-3">
           <h4 style="margin-top: 2rem;">Envio de shape zipado</h4>
           <div style="margin-top: 2rem; margin-bottom: 2rem;">
-            <input type="file">
+            <input type="file" ref="file" accept=".zip">
           </div>
           <div>
             <button type="button" class="btn btn-raised btn-primary" @click="sendShape()">Enviar</button>
@@ -43,8 +43,40 @@
       },
       initLayers() {},
       sendShape() {
-        console.log("TESTE");
+        let map = this.map;
         
+        let files = this.$refs.file.files;
+        if (files.length == 0) {
+          alert("Nothing sent!")
+          return; //do nothing if no file given yet
+        }
+
+        let file = files[0];
+
+        handleZipFile(file);
+
+        //More info: https://developer.mozilla.org/en-US/docs/Web/API/FileReader
+        function handleZipFile(file) {          
+          let reader = new FileReader();
+          reader.onload = function () {            
+            if (reader.readyState != 2 || reader.error) {
+              return;
+            } else {
+              convertToLayer(reader.result);
+            }
+          }
+          reader.readAsArrayBuffer(file);
+        }
+
+        function convertToLayer(buffer){
+          console.log("map", map);
+          
+          // console.log("buffer", shp(buffer));
+          shp(buffer).then(function(geojson){	//More info: https://github.com/calvinmetcalf/shapefile-js
+            var layer = L.shapefile(geojson).addTo(map);//More info: https://github.com/calvinmetcalf/leaflet.shapefile
+            console.log(layer);
+          });
+        }
       }
     },
     mounted() {
