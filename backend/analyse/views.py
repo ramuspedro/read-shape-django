ffc5f1eaf3d2f700c8e743b96f671163399cf4a6
Pedro Ramos <pedro.ramos@terras.agr.br>
2018-10-29 17:48:52 -0300

    working in rest shape

--- backend/analyse/views.py
+++ backend/analyse/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.core import serializers
+from rest_framework import generics
+
+from .models import ShapeUpload
+from .serializers import ShapeUploadSerializer
+
+class ShapeUploadRest(generics.CreateAPIView):
+  """ Test Upload Shape """
+  queryset = ShapeUpload.objects.all()
+  serializer_class = ShapeUploadSerializer
+
+class AnalyseDenseForest(generics.CreateAPIView):
+  """ Analyse Dense Forest """
